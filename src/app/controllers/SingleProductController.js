a0489bc4272b5dd97f19438c170f511697a42c24
tminhbao <tminhbao2001@gmail.com>
2022-01-13 15:44:22 +0700

    Thêm chức năng: Hiển thị các sản phẩm liên quan

--- src/app/controllers/SingleProductController.js
+++ src/app/controllers/SingleProductController.js
@@ -4,7 +4,14 @@ class SingleProductController {
     const productDetail = await ProductModel.getProductDetail(
       req.query.id || "laptop001"
     );
-    res.render("single-product", { listItem: productDetail, user: req.user });
+    const productRelevant = await ProductModel.getProductRelevant(
+      req.query.id || "laptop001"
+    );
+    res.render("single-product", {
+      listItem: productDetail,
+      user: req.user,
+      productRelevant: productRelevant,
+    });
   }
 }
 module.exports = new SingleProductController();
