88307095c2c8cd89ba512ab655954c32dd567d92
tminhbao <tminhbao2001@gmail.com>
2022-01-18 10:59:52 +0700

    Fix tìm kiếm nâng cao

--- src/models/product.js
+++ src/models/product.js
@@ -144,8 +144,16 @@ const getProductByPriceHighLow = async (pricehighlow, pagePriceHighLow) => {
 // Tìm kiếm sản phẩm và phân trang
 const getProductBySearching = async (name, pageSearching) => {
   const offset = (pageSearching - 1 || 1 - 1) * ITEM_PER_PAGE;
-  const sqlPaginate = `SELECT laptop.*, manufacture.manufacture_name as manu_name, manufacture.manufacture_id as manu_id, CONCAT_WS(" ", manufacture.manufacture_name, model.model_name, laptop.laptop_name) as name FROM laptop JOIN manufacture ON laptop.manufacture = manufacture.manufacture_id JOIN model ON laptop.model = model.model_id WHERE manufacture_name LIKE '%${name}%' OR model_name LIKE '%${name}%' OR laptop_name LIKE '%${name}%' LIMIT ${ITEM_PER_PAGE} OFFSET ${offset} `;
-  const sqlTotalItem = `SELECT COUNT(*) as totalItem FROM laptop,manufacture,model WHERE laptop.manufacture = manufacture.manufacture_id AND laptop.model = model.model_id AND manufacture.manufacture_name LIKE '%${name}%' OR model.model_name LIKE '%${name}%' OR laptop.laptop_name LIKE '%${name}%' `;
+  //const sqlPaginate = `SELECT laptop.*, manufacture.manufacture_name as manu_name, manufacture.manufacture_id as manu_id,
+  // CONCAT_WS(" ", manufacture.manufacture_name, model.model_name, laptop.laptop_name) as name
+  // FROM laptop JOIN manufacture ON laptop.manufacture = manufacture.manufacture_id JOIN model ON laptop.model = model.model_id
+  // WHERE manufacture_name LIKE '%${name}%' OR model_name LIKE '%${name}%' OR laptop_name LIKE '%${name}%' LIMIT ${ITEM_PER_PAGE} OFFSET ${offset} `;
+
+  const sqlPaginate = `SELECT laptop.*,manufacture.manufacture_name as manu_name, manufacture.manufacture_id as manu_id,CONCAT_WS(" ", manufacture.manufacture_name, model.model_name, laptop.laptop_name) as name 
+  FROM laptop JOIN manufacture ON laptop.manufacture = manufacture.manufacture_id JOIN model ON laptop.model = model.model_id WHERE CONCAT_WS(" ", manufacture.manufacture_name, model.model_name, laptop.laptop_name) LIKE '%${name}%' LIMIT ${ITEM_PER_PAGE} OFFSET ${offset} `;
+
+  //const sqlTotalItem = `SELECT COUNT(*) as totalItem FROM laptop,manufacture,model WHERE laptop.manufacture = manufacture.manufacture_id AND laptop.model = model.model_id AND manufacture.manufacture_name LIKE '%${name}%' OR model.model_name LIKE '%${name}%' OR laptop.laptop_name LIKE '%${name}%' `;
+  const sqlTotalItem = `SELECT COUNT(*) as totalItem FROM laptop JOIN manufacture ON laptop.manufacture = manufacture.manufacture_id JOIN model ON laptop.model = model.model_id WHERE CONCAT_WS(" ", manufacture.manufacture_name, model.model_name, laptop.laptop_name) LIKE '%${name}%'`;
   const [listProductSearching, totalItem] = await Promise.all([
     executeQuery(sqlPaginate),
     executeQuery(sqlTotalItem),
